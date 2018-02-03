0a2407f7804b52e536bc658654f94204acbc50f6
Midas Lambrichts <midas_lambrichts@hotmail.com>
2018-02-03 17:56:12 +0100

    Added structured data for Google Events

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,45 @@
 
 @section('content')
   <div class="container">
-  <div class="col-md-12"> 
-    <h3> Wedstrijden deze week </h3>
-    @include('includecomponents.pastgametable', ["matches" => $this_week_matches])
+    <h1>KBBC T&T Turnhout</h1>
+    <div class="col-md-12"> 
+      <h3> Wedstrijden deze week </h3>
+      @include('includecomponents.pastgametable', ["matches" => $this_week_matches])
+    </div>
+    <!-- Volgende week -->
+    <div class="col-md-6"> 
+      <h3> Wedstrijden volgende week </h3>
+      @include('includecomponents.futuregametable', ["matches" => $next_week_matches])
+    </div>
+    <!-- Vorige week -->
+    <div class="col-md-6"> 
+      <h3> Wedstrijden vorige week</h3>
+      @include('includecomponents.pastgametable', ["matches" => $prev_week_matches])
+    </div>
   </div>
-  <!-- Volgende week -->
-  <div class="col-md-6"> 
-    <h3> Wedstrijden volgende week </h3>
-    @include('includecomponents.futuregametable', ["matches" => $next_week_matches])
-  </div>
-  <!-- Vorige week -->
-  <div class="col-md-6"> 
-    <h3> Wedstrijden vorige week</h3>
-    @include('includecomponents.pastgametable', ["matches" => $prev_week_matches])
-  </div>
-  </div>
+@endsection
+
+@section('scripts')
+<script type="application/ld+json">
+  @foreach($this_week_matches as $match)
+    @php
+      $dt = date_create_from_format("d-m-Y G.i", $match->datumString." ".$match->beginTijd);
+      $startDate = date_format($dt, DateTime::ATOM)
+    @endphp
+    {
+      "@context": "http://schema.org",
+      "@type": "Event",
+      "name": "{{ $match->tTNaam}} VS {{ $match->tUNaam }}",
+      "location": {
+        "@type": "Place",
+        "name": "{{ $match->extraDetails->adress }}",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "{{ $match->extraDetails->adress }}"
+        }
+      },
+      "startDate": "{{ $startDate }}"
+    },
+  @endforeach
+</script>
 @endsection
